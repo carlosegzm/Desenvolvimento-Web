c19ec25998ec5dc0183b1b0722a2fd314212a5b2
Cadu <boli69112@gmail.com>
2025-09-10 17:39:05 -0300

    chore: Estrutura inicial do projeto

---
+++ src/main/java/br/com/filmix/api/FilmixApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.filmix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/filmix/api/FilmixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filmix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
